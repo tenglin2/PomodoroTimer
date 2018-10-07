44e446ff9b39aafe03dca94c5f5c5aa3e91c13c6
Teng Lin <tenglin2@gmail.com>
2018-10-06 20:49:26 -0400

    Making the timer work as an integer

--- script.js
+++ script.js
@@ -3,9 +3,10 @@
 //   shortButton.textContent = "Jump";
 //   shortButton.style.backgroundColor = "yellow";
 // });
-var shortTime = "10:00";
-var workTime = "45:00";
-var longTime = "20:00";
+var shortTime = 10;
+var workTime = 45;
+var longTime = 20;
+var realTime = 45;
 
 var currentTime = "work";
 
@@ -24,6 +25,7 @@ shortButton.addEventListener("click", (e) => {
   if (currentTime !== "short"){
     timerclock.textContent = shortTime;
     currentTime = "short";
+    realTime = shortTime;
   }
 
 });
@@ -31,6 +33,7 @@ workButton.addEventListener("click", (e) => {
   if (currentTime !== "work"){
     timerclock.textContent = workTime;
     currentTime = "work";
+    realTime = workTime;
   }
 
 });
@@ -38,6 +41,7 @@ longButton.addEventListener("click", (e) => {
   if (currentTime !== "long"){
     timerclock.textContent = longTime;
     currentTime = "long";
+    realTime = longTime;
   }
 });
 
@@ -64,4 +68,98 @@ editButton.addEventListener("click", (e) => {
     default:
       console.log("Something went wrong!");
   }
-})
+});
+
+var myInterval = -1;
+var actButton = document.getElementById("act");
+actButton.addEventListener("click", function(event){
+  // actButton.textContent = "Works";
+  // timerclock.textContent = parseInt(timerclock.textContent)-1;
+  if (myInterval == -1) {
+    actButton.innerHTML = "Pause";
+    myInterval = setInterval(function(){
+      realTime--;
+      if (realTime < 0){
+        actButton.innerHTML = "Start";
+        clearInterval(myInterval);
+        myInterval = -1;
+        switch (currentTime){
+          case "short":
+            realTime = shortTime;
+            break;
+          case "work":
+            realTime = workTime;
+            break;
+          case "long":
+            realTime = longTime;
+            break;
+          default:
+            console.log("Something went wrong here.");
+        }
+      }
+      timerclock.innerHTML = realTime;
+    }, 1000);
+  }else{
+    actButton.innerHTML = "Start";
+    clearInterval(myInterval);
+    myInterval = -1;
+  }
+});
+
+// var myInterval = -1;
+
+// var myInterval = -1;
+// var actButton = document.getElementById("act");
+// actButton.addEventListener("click", (e) => {
+//   console.log(myInterval)
+//   if (myInterval === -1){
+//     myInterval = setInterval(startTimer, 1000);
+//   }else{
+//     clearInterval(myInterval);
+//     myInterval = -1;
+//   }
+// })
+//
+// function startTimer(){
+//   console.log("runs")
+//   realTime--;
+//   if (realTime < 0){
+//     realTime == 45;
+//   }
+//   timerclock.textContent = realTime;
+// }
+//
+//
+// function startTimer(){
+//   actButton.addEventListener("click", (e) => {
+//     // if (myInterval === -1){
+//       // myInterval = setInterval(changeColor(), 1000);
+//     // }else{
+//     //   clearInterval(myInterval);
+//     // }
+//
+//     var myVar = setInterval(changeColor, 1000);
+//   });
+// }
+//
+//
+// function changeColor(){
+//   // if (startButton.style.backgroundColor === "lightblue"){
+//   //   startButton.style.backgroundColor = "lightgreen";
+//   // }else{
+//   //   startButton.style.backgroundColor = "lightblue";
+//   // }
+//   // alert("hello");
+//
+//   // var time = parseInt(timerclock.textContent) - 1;
+//   // timerclock.textContent = time;
+//
+//   actButton.style.Background = actButton.style.backgroundColor == "lightblue" ? "pink" : "lightblue";
+// }
+//
+// function stopColor(){
+//   actButton.addEventListener()
+//   clearInterval(myVar);
+// }
+//
+// startTimer();
