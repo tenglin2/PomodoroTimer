4971cf41d79cc67a81fa04ab50d8d9a8c7087abe
Teng Lin <tenglin2@gmail.com>
2018-10-10 18:55:15 -0400

    changed naming to camelCase and cleaned up some of the javascript

--- script.js
+++ script.js
@@ -3,45 +3,57 @@
 //   shortButton.textContent = "Jump";
 //   shortButton.style.backgroundColor = "yellow";
 // });
-var shortTime = 10;
-var workTime = 45;
-var longTime = 20;
-var realTime = 45;
+var shortTime = "10:00";
+var workTime = "45:00";
+var longTime = "20:00";
+var realTime = workTime;
 
 var currentTime = "work";
 
-// var timerclock = document.querySelector(".timer_container");
-// timerclock.addEventListener("click", (e) => {
-//   timerclock.style.backgroundColor = "yellow";
+
+// var timerClock = document.querySelector(".timer_container");
+// timerClock.addEventListener("click", (e) => {
+//   timerClock.style.backgroundColor = "yellow";
 // });
-var timerclock = document.querySelector("#timerclock");
-timerclock.textContent = workTime;
+var timerClock = document.querySelector("#timerClock");
+timerClock.textContent = workTime;
 
 var shortButton = document.getElementById("short");
 var workButton = document.getElementById("work");
 var longButton = document.getElementById("long");
 
+workButton.style.backgroundColor = "lightgreen";
+
 shortButton.addEventListener("click", (e) => {
   if (currentTime !== "short"){
-    timerclock.textContent = shortTime;
+    timerClock.textContent = shortTime;
     currentTime = "short";
     realTime = shortTime;
+    shortButton.style.backgroundColor = "lightgreen";
+    workButton.style.backgroundColor = "lightblue";
+    longButton.style.backgroundColor = "lightblue";
   }
 
 });
 workButton.addEventListener("click", (e) => {
   if (currentTime !== "work"){
-    timerclock.textContent = workTime;
+    timerClock.textContent = workTime;
     currentTime = "work";
     realTime = workTime;
+    workButton.style.backgroundColor = "lightgreen";
+    shortButton.style.backgroundColor = "lightblue";
+    longButton.style.backgroundColor = "lightblue";
   }
 
 });
 longButton.addEventListener("click", (e) => {
   if (currentTime !== "long"){
-    timerclock.textContent = longTime;
+    timerClock.textContent = longTime;
     currentTime = "long";
     realTime = longTime;
+    longButton.style.backgroundColor = "lightgreen";
+    shortButton.style.backgroundColor = "lightblue";
+    workButton.style.backgroundColor = "lightblue";
   }
 });
 
@@ -49,62 +61,102 @@ longButton.addEventListener("click", (e) => {
 // hello.textContent = "Mongoose";
 var newTime;
 
-var editButton = document.getElementById("edit");
-editButton.addEventListener("click", (e) => {
-  newTime = prompt("Please enter a new time in format xx:xx");
-  switch (currentTime){
-    case "short":
-      shortTime = newTime;
-      timerclock.textContent = shortTime;
-      break;
-    case "work":
-      workTime = newTime;
-      timerclock.textContent = workTime;
-      break;
-    case "long":
-      longTime = newTime;
-      timerclock.textContent = longTime;
-      break;
-    default:
-      console.log("Something went wrong!");
-  }
-});
-
-var myInterval = -1;
-var actButton = document.getElementById("act");
-actButton.addEventListener("click", function(event){
-  // actButton.textContent = "Works";
-  // timerclock.textContent = parseInt(timerclock.textContent)-1;
-  if (myInterval == -1) {
-    actButton.innerHTML = "Pause";
-    myInterval = setInterval(function(){
-      realTime--;
-      if (realTime < 0){
-        actButton.innerHTML = "Start";
-        clearInterval(myInterval);
-        myInterval = -1;
-        switch (currentTime){
-          case "short":
-            realTime = shortTime;
-            break;
-          case "work":
-            realTime = workTime;
-            break;
-          case "long":
-            realTime = longTime;
-            break;
-          default:
-            console.log("Something went wrong here.");
-        }
+function editButtonListener(){
+  editButton.addEventListener("click", (e) => {
+    validTime = false;
+    while(validTime === false){
+      newTime = prompt("Please enter a new time in format xx:xx");
+      if (newTime.length !== 5 || newTime[2] !== ":" || newTime[0] >= 6 || newTime[3] >= 6){
+        alert("Your input fucking sucks, do it again.")
+      }else{
+        validTime = true;
       }
-      timerclock.innerHTML = realTime;
-    }, 1000);
-  }else{
-    actButton.innerHTML = "Start";
-    clearInterval(myInterval);
-    myInterval = -1;
-  }
-});
+    }
+
+    switch (currentTime){
+      case "short":
+        shortTime = newTime;
+        timerClock.textContent = shortTime;
+        break;
+      case "work":
+        workTime = newTime;
+        timerClock.textContent = workTime;
+        break;
+      case "long":
+        longTime = newTime;
+        timerClock.textContent = longTime;
+        break;
+      default:
+        console.log("Something went wrong!");
+    }
+  });
+}
+var editButton = document.getElementById("edit");
+
+
+
+var actButton = document.getElementById("act");
+
+function actButtonListener(){
+  actButton.addEventListener("click", function(event){
+    var myInterval = -1;
+    if (myInterval == -1) {
+      actButton.innerHTML = "Pause";
+      myInterval = setInterval(function(){
+        console.log(realTime);
+        var minutes, seconds = realTime.split(":");
+        minutes = parseInt(minutes);
+        seconds = parseInt(seconds);
+        console.log(minutes);
+        console.log(typeof minutes)
+        if(minutes === 0 && seconds < 0){
+          actButton.innerHTML = "Start";
+          clearInterval(myInterval);
+          myInterval = -1;
+
+          switch (currentTime){
+              case "short":
+                realTime = shortTime;
+                break;
+              case "work":
+                realTime = workTime;
+                break;
+              case "long":
+                realTime = longTime;
+                break;
+              default:
+                console.log("Something went wrong here.");
+          }
+          timerClock.innerHTML
+        }
+        // if (realTime < 0){
+        //   actButton.innerHTML = "Start";
+        //   clearInterval(myInterval);
+        //   myInterval = -1;
+        //   switch (currentTime){
+        //     case "short":
+        //       realTime = shortTime;
+        //       break;
+        //     case "work":
+        //       realTime = workTime;
+        //       break;
+        //     case "long":
+        //       realTime = longTime;
+        //       break;
+        //     default:
+        //       console.log("Something went wrong here.");
+        //   }
+        // }
+        timerClock.innerHTML = realTime;
+      }, 1000);
+    }else{
+      actButton.innerHTML = "Start";
+      clearInterval(myInterval);
+      myInterval = -1;
+    }
+  });
+}
+
 
 // var myInterval = -1;
 
@@ -126,7 +178,7 @@ actButton.addEventListener("click", function(event){
 //   if (realTime < 0){
 //     realTime == 45;
 //   }
-//   timerclock.textContent = realTime;
+//   timerClock.textContent = realTime;
 // }
 //
 //
@@ -151,8 +203,8 @@ actButton.addEventListener("click", function(event){
 //   // }
 //   // alert("hello");
 //
-//   // var time = parseInt(timerclock.textContent) - 1;
-//   // timerclock.textContent = time;
+//   // var time = parseInt(timerClock.textContent) - 1;
+//   // timerClock.textContent = time;
 //
 //   actButton.style.Background = actButton.style.backgroundColor == "lightblue" ? "pink" : "lightblue";
 // }
@@ -163,3 +215,6 @@ actButton.addEventListener("click", function(event){
 // }
 //
 // startTimer();
+
+editButtonListener();
+actButtonListener();
